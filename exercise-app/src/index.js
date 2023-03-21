02b1df570ad26b266350d1e880469f0226502104
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-21 17:47:37 +0300

    added exercise

--- exercise-app/src/index.js
+++ exercise-app/src/index.js
@@ -5,7 +5,6 @@ function getName (name) {
     return <h1>Hello, {name}</h1>
 }
 
-ReactDOM.render(getName(), root)
+ReactDOM.render(getName(<p>Adil</p>), root)
 
-// if I understand the question correctly, if you don't pass the value of the name variable to the function, 
-// it will return only "Hello," without a name
+// if I understand the question correctly, this will work but the name will wrap to a new line
