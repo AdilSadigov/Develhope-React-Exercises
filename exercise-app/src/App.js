98d7ae9a2c54645fadb39d5b742c1d19b37b7583
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-22 17:08:37 +0300

    added Props-4 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -3,6 +3,8 @@ import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render () {
-        return <Welcome name = "Adil" age = {23}/>
+        return <Welcome name = {<strong>Adil</strong>} age = {23}/>
     }
 }
+
+// If the prop is a JSX expression instead of a string, you need to use curly braces
