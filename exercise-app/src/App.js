29662cb481e26521e177e0bf26ab530771ef79e1
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-22 17:03:19 +0300

    added Props-3 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 
-Welcome.defaultProps = {
-    name: "Adil"
-}
-
 export class App extends React.Component {
     render () {
-        return <Welcome />
+        return <Welcome name = "Adil" age = {23}/>
     }
 }
