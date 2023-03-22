041f8351db3190a5356b29d53b0ca3839bb48727
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-22 15:32:38 +0300

    added Component-3 exercise

--- exercise-app/src/index.js
+++ exercise-app/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-import { Hello } from './Hello';
+import { App } from './App';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<Hello /> ,root)
+ReactDOM.render(<App /> ,root)
