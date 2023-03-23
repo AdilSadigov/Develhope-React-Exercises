7a7e89a315a030fb74e7e9e8f7a52d624860c816
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-23 20:59:55 +0300

    added Forms-2 exercise

--- exercise-app/src/InteractiveWelcome.js
+++ exercise-app/src/InteractiveWelcome.js
@@ -3,21 +3,27 @@ import { Welcome } from "./Welcome";
 
 export class InteractiveWelcome extends React.Component {
     state = {
-        username: 'Adil'
+        username: 'Adil',
+        password: '',
+        remember: false
     }
 
     handleUsernameChange = (event) => {
         const name = event.target.name
         const value = event.target.value
+        const type = event.target.type
+        const checked = event.target.checked
 
         this.setState ({
-            [name]: value
+            [name]: type === "checkbox" ? checked : value
         })
     }
 
     render() {
         return <div>
             <input name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
+            <input name="password" type="password" value={this.state.password} onChange={this.handleUsernameChange} style={{margin: '0px 10px'}}/>
+            <input name="remember" type="checkbox" value={this.state.remember} onChange={this.handleUsernameChange}/>
             <Welcome name={this.state.username}/>
         </div>
     }
