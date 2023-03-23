9fef6f1d430d3f1f79e1cdd6e19ec3d362c4ea61
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-24 00:59:35 +0300

    added List-5 exercise

--- exercise-app/src/TodoList.js
+++ exercise-app/src/TodoList.js
@@ -1,5 +1,3 @@
-// Already done 
-
 import React from "react";
 
 export class TodoList extends React.Component {
@@ -25,11 +23,18 @@ export class TodoList extends React.Component {
         })
     }
 
+    removeItems = () => {
+        this.setState ({
+            items: []
+        })
+    }
+
     render () {
         return (
             <div>
                 <input name="todoInput" value={this.state.input} onChange={this.handleChange}/>
                 <button onClick={this.addItem} style={{marginLeft: '10px'}}>Add to list</button>
+                <button onClick={this.removeItems} style={{marginLeft: '10px'}}>Reset</button>
                 <ul>
                     {this.state.items.map((item, index) => (<li key={index}>{item}</li>))}
                 </ul>
