d92e46e72998ecc3a908daf53def7c77858cb614
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-24 01:23:33 +0300

    added List-6 exercise

--- exercise-app/src/TodoList.js
+++ exercise-app/src/TodoList.js
@@ -23,10 +23,16 @@ export class TodoList extends React.Component {
         })
     }
 
-    removeItems = () => {
+    removeAllItems = () => {
         this.setState ({
             items: []
         })
+    }
+
+    removeItem = (index) => {
+        this.setState ((prev) => ({
+            items: prev.items.filter((item, i) => i != index)
+        }))
     }
 
     render () {
@@ -34,9 +40,15 @@ export class TodoList extends React.Component {
             <div>
                 <input name="todoInput" value={this.state.input} onChange={this.handleChange}/>
                 <button onClick={this.addItem} style={{marginLeft: '10px'}}>Add to list</button>
-                <button onClick={this.removeItems} style={{marginLeft: '10px'}}>Reset</button>
+                <button onClick={this.removeAllItems} style={{marginLeft: '10px'}}>Reset</button>
+                <h3>Todo List:</h3>
                 <ul>
-                    {this.state.items.map((item, index) => (<li key={index}>{item}</li>))}
+                    {this.state.items.map((item, index) => (
+                        <li key={index} style={{marginBottom: '8px'}}>
+                            {item} 
+                            <button onClick={() => this.removeItem(index)} style={{marginLeft: '10px'}}>Remove</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         )
