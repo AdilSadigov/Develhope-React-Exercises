2270e5e8cad89862d9c837b3d1d16233071956dd
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-23 22:03:35 +0300

    added Forms-4 exercise

--- exercise-app/src/Login.js
+++ exercise-app/src/Login.js
@@ -21,12 +21,21 @@ export class Login extends React.Component {
         })   
     }
 
+    handleResetState = () => {
+        this.setState ({
+            username: '',
+            password: '',
+            remember: false
+        })
+    }
+
     render() {
         return <div>
             <input name="username" value={this.state.username} onChange={this.handleInputChange}/>
             <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} style={{margin: '0px 10px'}}/>
             <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange}/> 
             <button disabled={this.state.disabled} onClick={this.onLogin} style={{marginLeft:'10px'}}>Login</button>
+            <button onClick={this.handleResetState} style={{marginLeft:'10px'}}>Reset</button>
         </div>
     }
 }
