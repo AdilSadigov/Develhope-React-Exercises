d4cf8787e204dc1d4760a759fbed28e8eaffe341
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-26 00:23:40 +0300

    added useState-2 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -1,34 +1,23 @@
 import React from "react";
 import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
-import { Colors } from "./Colors";
-import { Container } from "./Container";
 import { Counter } from "./Counter";
-import { DisplayLanguage } from "./DisplayLanguage";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
-import { TodoList } from "./TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Welcome } from "./Welcome";
-import { LanguageContext } from "./LanguageContext";
-import { Sum } from "./Sum";
 
 export class App extends React.Component {
-    state = {
-        language: 'en'
+    constructor(props) {
+        super(props);
+        this.onLogin = this.onLogin.bind(this);
     }
 
-    handleLanguageChange = (event) => {
-        this.setState ({
-            language: event.target.value
-        })
+    onLogin(username, password, event) {
+        event.preventDefault();
+        console.log(username, password);
     }
 
     render () {
-        return (
-            <div>
-                <ClickCounter />
-            </div>
-        )
+        return <Login onLogin={this.onLogin} />
     }
 }
--- exercise-app/src/Login.js
+++ exercise-app/src/Login.js
@@ -1,48 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Login extends React.Component {
-    state = {
-        username: '',
-        password: '',
-        remember: false,
-        disabled: true
-    }
+export function Login({onLogin}) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [remember, setRemember] = useState(false)
+    const [disabled, setDisabled] = useState(true)
 
-    handleInputChange = (event) => {
-        
+    const handleInputChange = (event) => {       
         const name = event.target.name
         const value = event.target.value
-        const type = event.target.type
         const checked = event.target.checked
 
-        this.setState ({
-            [name]: type === "checkbox" ? checked : value,
-            disabled: (this.state.username == '' || this.state.password == '') ? true : false,
-        })   
-    }
-
-    handleResetState = () => {
-        this.setState ({
-            username: '',
-            password: '',
-            remember: false
-        })
-    }
-
-    render() {
-        const LoginBtn = {
-            background: this.state.password.length < 8 ? 'red' : 'green',
-            marginLeft:'10px'
+        if (name === 'username') {
+            setUsername(value);
+        } 
+        else if (name === 'password') {
+            setPassword(value);
+        } 
+        else if (name === 'remember') {
+            setRemember(checked);
         }
-
-        console.log(this.state.password.length)
-
-        return <div>
-            <input name="username" value={this.state.username} onChange={this.handleInputChange}/>
-            <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} style={{margin: '0px 10px'}}/>
-            <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange}/> 
-            <button disabled={this.state.disabled} onClick={this.onLogin} style={LoginBtn}>Login</button>
-            <button onClick={this.handleResetState} style={{marginLeft:'10px'}}>Reset</button>
-        </div>
+      
+        setDisabled(username === '' || password === '');   
     }
+        return <div>
+            <input name="username" value={username} onChange={handleInputChange}/>
+            <input name="password" type="password" value={password} onChange={handleInputChange} style={{margin: '0px 10px'}}/>
+            <input name="remember" type="checkbox" checked={remember} onChange={handleInputChange}/> 
+            <button disabled={disabled} onClick={(event) => {onLogin(username, password, event)}} style={{marginLeft:'10px'}}>Login</button>
+        </div>
 }
