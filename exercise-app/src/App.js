081f9bafbc1d7812c1e90cc5d983f02fbcdd08dd
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-26 17:03:44 +0300

    added useEffect-3 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Counter } from "./Counter";
+import { GithubUser } from "./GithubUser";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
 import { Welcome } from "./Welcome";
 
 export function App() {
-    return <Counter/>
+    return <GithubUser username="AdilSadigov"/>
 }
