9dc7153439a8988bcda9260cdd5874a9bd74f13a
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-26 22:06:52 +0300

    added useRef-1 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -9,16 +9,16 @@ import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
 import { Welcome } from "./Welcome";
 
-const users = [
-   {id: 1, name: 'Person1', age: 22},
-   {id: 2, name: 'Person2', age: 28},
-   {id: 3, name: 'Person3', age: 16},
-   {id: 4, name: 'Person4', age: 14},
-   {id: 5, name: 'Person5', age: 40},
-   {id: 6, name: 'Person6', age: 17},
-   {id: 7, name: 'Person7', age: 20},
-];
+// const users = [
+//    {id: 1, name: 'Person1', age: 22},
+//    {id: 2, name: 'Person2', age: 28},
+//    {id: 3, name: 'Person3', age: 16},
+//    {id: 4, name: 'Person4', age: 14},
+//    {id: 5, name: 'Person5', age: 40},
+//    {id: 6, name: 'Person6', age: 17},
+//    {id: 7, name: 'Person7', age: 20},
+// ];
 
 export function App() {
-    return <FilteredList list={users}/>
+    return <Login/>
 }
--- exercise-app/src/Login.js
+++ exercise-app/src/Login.js
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLogin } from "./useLogin";
 
 export function Login({onLogin}) {
     const { username, password, onUsernameChange, onPasswordChange } = useLogin();
-    const [remember, setRemember] = useState(false);
+    const [remember, setRemember] = useState(false);    
+    const inputRef = useRef()
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
 
     const handleRememberChange  = (event) => {
         setRemember(event.target.checked);
     }
 
     return <div>
-        <input name="username" value={username} onChange={onUsernameChange} />
+        <input ref={inputRef} name="username" value={username} onChange={onUsernameChange} />
         <input name="password" type="password" value={password} onChange={onPasswordChange} style={{ margin: '0px 10px' }} />
         <input name="remember" type="checkbox" checked={remember} onChange={handleRememberChange} />
         <button onClick={(event) => {onLogin(username, password, event)}} disabled={!username || !password} style={{ marginLeft: '10px' }}>Login</button>
