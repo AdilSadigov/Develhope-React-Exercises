97332df1211ece0a29cf532a2805f61e637c72ab
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-26 18:03:32 +0300

    added useEffect-4 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -3,10 +3,11 @@ import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Counter } from "./Counter";
 import { GithubUser } from "./GithubUser";
+import { GithubUserList } from "./GithubUserList";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
 import { Welcome } from "./Welcome";
 
 export function App() {
-    return <GithubUser username="AdilSadigov"/>
+    return <GithubUserList/>
 }
---
+++ exercise-app/src/GithubUserList.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { GithubUser } from "./GithubUser"
+
+export function GithubUserList() {
+
+    const [usernames, setUsernames] = useState([])
+
+    function handleAddNewUser(event) {
+        event.preventDefault()
+        const input = event.target.elements.username
+        const newUsername = input.value.trim()
+        if (newUsername) {
+            setUsernames([...usernames, newUsername])
+            input.value = ''
+        }
+
+        console.log(usernames)
+    }
+
+    return <div>
+        <form onSubmit={handleAddNewUser}>
+            <input name="username"/>
+            <button type="submit">Add new user</button>
+        </form>
+        <ul>
+            {usernames.map((username, index) => (
+            <li key={index}>
+                <GithubUser username={username} />
+            </li>
+            ))}
+        </ul>
+    </div>
+} 
