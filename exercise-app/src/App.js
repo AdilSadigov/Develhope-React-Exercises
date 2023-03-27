dbe485ec3092f5bfd11b6bec1da203975900d4f5
Adil Sadigov <sadiqovadil9@gmail.com>
2023-03-27 16:16:39 +0300

    added React-Router-2 exercise

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -12,6 +12,7 @@ import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Welcome } from "./Welcome";
 import { LanguageContext } from "./LanguageContext";
 import { Route, Routes } from "react-router-dom";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
 
@@ -19,6 +20,7 @@ export function App() {
         <Routes>
             <Route path="/" element={<Welcome name="Adil"/>} />
             <Route path="/counter" element={<Counter/>} />
+            <Route path="/users/:username" element={<ShowGithubUser />} />
         </Routes>
     )
 }
