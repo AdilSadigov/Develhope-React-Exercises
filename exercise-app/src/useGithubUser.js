87839dd65da6517349e091bc4a1d3e12aacb2904
Adil Sadigov <sadiqovadil9@gmail.com>
2023-05-05 16:40:17 +0300

    added exercise

--- exercise-app/src/useGithubUser.js
+++ exercise-app/src/useGithubUser.js
@@ -1,7 +1,12 @@
 import useSWR from 'swr'
 
 export function useGithubUser(username) {
-    const { data, error, mutate } = useSWR(`https://api.github.com/users/${username}`)
+    let url = `https://api.github.com/users/${username}`
+    if (username == null || username == undefined) {
+        url = null
+    }
+    
+    const { data, error, mutate } = useSWR(url)
 
     function fetchGithubUser() {
         mutate()
